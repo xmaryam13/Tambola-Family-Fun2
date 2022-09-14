7972de43857575505f3b0fabc9b9a1e3ff9bb01e
xmaryam13 <90093129+xmaryam13@users.noreply.github.com>
2022-09-14 20:11:50 +0100

    Update tambola_client1.py

--- tambola_client1.py
+++ tambola_client1.py
@@ -7,13 +7,18 @@ from PIL import ImageTk, Image
 screen_width = None
 screen_height = None
 SERVER = None
-PORT = None
-IP_ADDRESS = None
+PORT = 8000
+IP_ADDRESS = '127.0.0.1'
 playerName = None
 nameWindow = None
 nameEntry = None
 canvas1 = None
 button = None
+
+ticketGrid = [] 
+currentNumberList = [] 
+flashNumberList = [] 
+flashNumberLabel = None
 
 def receivedMsg():
     pass
@@ -23,15 +28,12 @@ def setup():
     global SERVER
     global PORT
     global IP_ADDRESS
-    
-    PORT = 6000
-    IP_ADDRESS = '127.0.0.1'
+
     
     SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     SERVER.connect((IP_ADDRESS,PORT))
     
-    thread = Thread(target= receivedMsg)
-    thread.start()
+
     
     
 def saveName():
